03ec0a4468b51d7b8d5080c88d23bf810e7bc2f3
YAGOPOP <ttatarinov50@gmail.com>
2026-09-14 21:41:40 +0300

    add pager and editor env vars

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,4 +21,10 @@ if status is-interactive
     alias ls "eza -l"
 
     bind ctrl-я undo
+
+    set -x EDITOR micro
+    set -x PAGER "bat --paging=auto -p"
+
+    set -x MANROFFOPT "-c" # Не использовать цветные/терминальные escape-последовательности для вывода.
+    set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 end
